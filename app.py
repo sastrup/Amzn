c71258dc23d994d3cd4b1df38719fee5500fa3a7
astrsea <Sean.Astrup@chrobinson.com>
2018-01-08 13:19:09 -0600

    Add buy box merchant

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ datestamp = timestamp.strftime('%Y%m%d')
 
 amazon = bottlenose.Amazon(amazonPublicKey, amazonSecretKey, amazonAssociateID)
 
+
 class book(object):
 
     def __init__(self, ItemID='9780134475585'):  # '9781101873724'):
@@ -19,7 +20,7 @@ class book(object):
         self.NameSpace = {'ns0': 'http://webservices.amazon.com/AWSECommerceService/2013-08-01'}
         self.IdType = 'ISBN'
         self.SearchIndex = 'Books'
-        self.ResponseGroup = 'Offers'
+        self.ResponseGroup = 'OfferFull'
         self.XMLResponse = amazon.ItemLookup(ItemId=self.ItemID,
                                              IdType=self.IdType,
                                              SearchIndex=self.SearchIndex,
@@ -33,8 +34,10 @@ class book(object):
                                                 namespaces=self.NameSpace)[0].text
         self.LowestUsedPrice = self.root.findall('ns0:Items/ns0:Item/ns0:OfferSummary/ns0:LowestUsedPrice/ns0:Amount',
                                                  namespaces=self.NameSpace)[0].text
+        self.BuyBoxMerchant = self.root.findall('ns0:Items/ns0:Item/ns0:Offers/ns0:Offer/ns0:Merchant/ns0:Name',
+                                                namespaces=self.NameSpace)[0].text
 
 
 test = book()
 
-print(test.TotalUsedOffers)
+print(test.BuyBoxMerchant)
