92450a5c5ad371ef7a1df348482cb17d459c25f1
astrsea <Sean.Astrup@chrobinson.com>
2018-01-08 15:35:26 -0600

    Add item attribute information

--- app.py
+++ app.py
@@ -15,36 +15,59 @@ amazon = bottlenose.Amazon(amazonPublicKey, amazonSecretKey, amazonAssociateID)
 
 class book(object):
 
-    def __init__(self, ItemID='9780134475585'):  # '9781101873724'):
+    def __init__(self, ItemID='0134475585'):#'9780134475585'):  # '9781101873724'):
         self.ItemID = ItemID
         self.NameSpace = {'ns0': 'http://webservices.amazon.com/AWSECommerceService/2013-08-01'}
         self.IdType = 'ISBN'
         self.SearchIndex = 'Books'
         self.OfferResponseGroup = 'OfferFull'
+
+        ' Creates element tree for getting offer information for the item'
         self.OfferXMLResponse = amazon.ItemLookup(ItemId=self.ItemID,
                                              IdType=self.IdType,
                                              SearchIndex=self.SearchIndex,
                                              ResponseGroup=self.OfferResponseGroup)
         self.OfferRoot = ET.fromstring(self.OfferXMLResponse)
+
+        ' Creates element tree for getting sales rank/ASIN of the item'
         self.RankXMLResponse = amazon.ItemLookup(ItemId=self.ItemID,
                                                   IdType=self.IdType,
                                                   SearchIndex=self.SearchIndex,
                                                   ResponseGroup='SalesRank')
         self.RankRoot = ET.fromstring(self.RankXMLResponse)
+
+        ' Creates element tree for getting item attributes'
+        self.ItemXMLResponse = amazon.ItemLookup(ItemId=self.ItemID,
+                                                 IdType=self.IdType,
+                                                 SearchIndex=self.SearchIndex,
+                                                 ResponseGroup='ItemAttributes')
+        self.ItemRoot = ET.fromstring(self.ItemXMLResponse)
+
         self.TotalNewOffers = self.OfferRoot.findall('ns0:Items/ns0:Item/ns0:OfferSummary/ns0:TotalNew',
-                                                namespaces=self.NameSpace)[0].text
+                                                     namespaces=self.NameSpace)[0].text
         self.TotalUsedOffers = self.OfferRoot.findall('ns0:Items/ns0:Item/ns0:OfferSummary/ns0:TotalUsed',
-                                                 namespaces=self.NameSpace)[0].text
+                                                      namespaces=self.NameSpace)[0].text
         self.LowestNewPrice = self.OfferRoot.findall('ns0:Items/ns0:Item/ns0:OfferSummary/ns0:LowestNewPrice/ns0:Amount',
-                                                namespaces=self.NameSpace)[0].text
+                                                     namespaces=self.NameSpace)[0].text
         self.LowestUsedPrice = self.OfferRoot.findall('ns0:Items/ns0:Item/ns0:OfferSummary/ns0:LowestUsedPrice/ns0:Amount',
-                                                 namespaces=self.NameSpace)[0].text
+                                                      namespaces=self.NameSpace)[0].text
         self.BuyBoxMerchant = self.OfferRoot.findall('ns0:Items/ns0:Item/ns0:Offers/ns0:Offer/ns0:Merchant/ns0:Name',
-                                                namespaces=self.NameSpace)[0].text
+                                                     namespaces=self.NameSpace)[0].text
         self.ASIN = self.RankRoot.findall('ns0:Items/ns0:Item/ns0:ASIN', namespaces=self.NameSpace)[0].text
         self.SalesRank = self.RankRoot.findall('ns0:Items/ns0:Item/ns0:SalesRank', namespaces=self.NameSpace)[0].text
-
+        self.Author = self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:Author', namespaces=self.NameSpace)[0].text
+        self.BookBinding = self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:Binding', namespaces=self.NameSpace)[0].text
+        self.Publisher = self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:Publisher', namespaces=self.NameSpace)[0].text
+        self.Pages = self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:NumberOfPages', namespaces=self.NameSpace)[0].text
+        self.PublicationDate = self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:PublicationDate', namespaces=self.NameSpace)[0].text
+        self.ListPrice = self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:ListPrice/ns0:Amount', namespaces=self.NameSpace)[0].text
+        # self.Weight = self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:ItemDimensions/ns0:Weight', namespaces=self.NameSpace)[0].text
+        self.Height = int(self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:ItemDimensions/ns0:Height', namespaces=self.NameSpace)[0].text) / 100.0
+        self.Length = int(self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:ItemDimensions/ns0:Length', namespaces=self.NameSpace)[0].text) / 100.0
+        self.Width = int(self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:ItemDimensions/ns0:Width', namespaces=self.NameSpace)[0].text) / 100.0
+        self.Title = self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:Title', namespaces=self.NameSpace)[0].text
+        self.AmazonTradeValue = self.ItemRoot.findall('ns0:Items/ns0:Item/ns0:ItemAttributes/ns0:TradeInValue/ns0:Amount', namespaces=self.NameSpace)[0].text
 
 test = book()
 
-print(test.SalesRank)
+print(test.AmazonTradeValue)
